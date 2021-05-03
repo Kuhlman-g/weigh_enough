d70db6e1dab538767fd41f7599c17d84c6469d10
kuhlman-g <kuhlman.gregory@gmail.com>
2021-05-03 12:15:09 -0400

    added base styling and routing buttons to home page

---
+++ app/controllers/rowers_controller.rb
@@ -0,0 +1,7 @@
+class RowersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
 
-  get '/boats', to: 'homes#index'
-
   resources :boats, only: [ :index, :show ]
+  resources :rowers, only: [ :index, :show ]
+
 
 end
