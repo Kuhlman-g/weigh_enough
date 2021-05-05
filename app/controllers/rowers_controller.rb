1443b7c31bf9dbc6156392351c68478f2c220687
kuhlman-g <kuhlman.gregory@gmail.com>
2021-05-04 22:40:30 -0400

    added rower validation anderror flash

--- app/controllers/rowers_controller.rb
+++ app/controllers/rowers_controller.rb
@@ -4,4 +4,21 @@ class RowersController < ApplicationController
     @rower = Rower.new
   end
 
+  def create
+    @rower = Rower.new(rower_params)
+
+    if @rower.save
+      flash.now[:notice] = "Rower Added to Roster!"
+    else
+      flash.now[:error] = "#{@rower.errors.full_messages.to_sentence}"
+      render :new
+    end
+  end
+  
+  private
+
+  def rower_params
+    params.require(:rower).permit(:first_name, :last_name, :height, :weight, :side, :twok_pr, :sixk_pr, :notes)
+  end
+
 end
--- app/models/rower.rb
+++ app/models/rower.rb
@@ -1,2 +1,6 @@
 class Rower < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :height, presence: true
+  validates :weight, presence: true
 end
