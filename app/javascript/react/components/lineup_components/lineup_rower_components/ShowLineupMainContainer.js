e12106ffa944acd5560c7aca5dc4599d8e55f805
kuhlman-g <kuhlman.gregory@gmail.com>
2021-05-24 09:19:13 -0400

    added logic for show table fetch

--- app/javascript/react/components/lineup_components/lineup_rower_components/ShowLineupMainContainer.js
+++ app/javascript/react/components/lineup_components/lineup_rower_components/ShowLineupMainContainer.js
@@ -21,9 +21,31 @@ const ShowLineupMainContainer = props => {
       setSelectedLineup(id)
     }
   }
+
+  const getLineups = async () => {
+    debugger
+    try {
+      const response = await fetch(`/api/v1/lineups/${lineupId}`)
+      if (!response){
+        const errorMessage = `${response.status} (${response.statusTest})`
+        const error = new Error(errorMessage)
+        throw(error)
+      }
+      const parsedLineups = await response.json()
+      setLineup(parsedLineups)
+    } catch(err){
+      console.error(`Error in fetch: ${err.message}`)
+    }
+  }
+
+  useEffect( ()=>{
+    getLineups()
+  }, [])
+
+
   return(
     <div>
-      <div>
+      <div onClick={getLineups()}>
         <ShowLineupRosterContainer lineups={lineup} handleLineupClick={handleLineupClick} />
       </div>
       <div>
--- app/javascript/react/components/lineup_components/lineup_rower_components/ShowLineupShowContainer.js
+++ app/javascript/react/components/lineup_components/lineup_rower_components/ShowLineupShowContainer.js
@@ -9,27 +9,6 @@ const ShowLineupShowContainer = (props) => {
     rowers: []
   })
 
-  const lineupId = props.id
-
-  const getLineups = async () => {
-    debugger
-    try {
-      const response = await fetch(`/api/v1/lineups/${lineupId}`)
-      if (!response){
-        const errorMessage = `${response.status} (${response.statusTest})`
-        const error = new Error(errorMessage)
-        throw(error)
-      }
-      const parsedLineups = await response.json()
-      setLineup(parsedLineups)
-    } catch(err){
-      console.error(`Error in fetch: ${err.message}`)
-    }
-  }
-
-  useEffect( ()=>{
-    getLineups()
-  }, [])
 
   return(
     <div>
